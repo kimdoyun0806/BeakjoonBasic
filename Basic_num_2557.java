a1090e24b54ca0b92bb9a101cc72952230691e73
kimdoyun0806 <gim_doyun@naver.com>
2024-12-17 02:16:30 +0900

    Hello World 출력하기

---
+++ Basic_num_2557.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Basic_num_2557 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("Hello World!");
+        bw.flush();
+        bw.close();
+    }
+}
