4a41a3fca1882ee94d51b99d6965b128174b68d3
kimdoyun0806 <gim_doyun@naver.com>
2024-12-22 16:11:39 +0900

    그대로 출력하기

---
+++ String/Basic_num_11718.java
@@ -0,0 +1,13 @@
+package String;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Basic_num_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        while((str = br.readLine()) != null )
+            System.out.println(str);
+    }
+}
