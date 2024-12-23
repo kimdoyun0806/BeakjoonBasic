14ca1e81d372ee50cd7c53d9176cc4bdd457ce4d
kimdoyun0806 <gim_doyun@naver.com>
2024-12-23 10:30:14 +0900

    2차원 배열

--- Array2/Array2_num_2738.java
+++ Array2/Array2_num_2738.java
@@ -1,4 +1,40 @@
 package Array2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Array2_num_2738 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
+        int[][] A = new int[N][M];
+        int[][] B = new int[N][M];
+
+        // 핼렬 A에 대한 값 넣기
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        // 행렬 B에 대한 값 넣기
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        // 행렬 A + B 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(A[i][j] + B[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
