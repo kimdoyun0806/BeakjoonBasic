a454d5341c028e92f9263f091e063321fcfb7572
kimdoyun0806 <gim_doyun@naver.com>
2025-01-22 23:58:50 +0900

    약수

---
+++ Deepening2/Deepening2_num_1037.java
@@ -0,0 +1,32 @@
+package Deepening2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Deepening2_num_1037 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            int M = Integer.parseInt(st.nextToken());
+            arr[i] = M;
+        }
+
+        for (int i=0; i< arr.length; i++) {
+            max = Math.max(max,arr[i]);
+            min = Math.min(min,arr[i]);
+
+        }
+        System.out.println(max*min);
+    }
+}
