89baaf0f7b844724238216ab6fe7c9e9ff0b5553
kimdoyun0806 <gim_doyun@naver.com>
2025-11-24 21:35:15 +0900

    N번째 큰수

---
+++ DataStructure2/num_2075.java
@@ -0,0 +1,25 @@
+package DataStructure2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class num_2075 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for(int i=0; i<N-1; i++) {
+            pq.poll();
+        }
+        System.out.println(pq.peek());
+
+    }
+}
