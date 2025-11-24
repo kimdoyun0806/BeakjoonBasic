47168395bb1912e96f5d2c210694a64b7fa11003
kimdoyun0806 <gim_doyun@naver.com>
2025-11-24 21:10:22 +0900

    문자열 집합

--- DataStructure2/num_14425.java
+++ DataStructure2/num_14425.java
@@ -1,4 +1,30 @@
 package DataStructure2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 public class num_14425 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
+
+        for (int i = 0; i < M; i++) {
+            if(set.contains(br.readLine())) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 }
+
