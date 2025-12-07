380bbbcdbdd56af9cde9489f24ccff4c12582d06
kimdoyun0806 <gim_doyun@naver.com>
2025-12-07 14:51:39 +0900

    힙힙

---
+++ DataStructure2/num_11279.java
@@ -0,0 +1,30 @@
+package DataStructure2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class num_11279 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(num);
+            }
+        }
+        System.out.println(sb);
+    }
+}
