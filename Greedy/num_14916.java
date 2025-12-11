b0c409d58d76c2fa1d9d9672d25d7af88db64087
kimdoyun0806 <gim_doyun@naver.com>
2025-12-11 17:13:29 +0900

    거스름돈

---
+++ Greedy/num_14916.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class num_14916 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        while (N>=0) {
+            if (N % 5 == 0) {
+                cnt += N / 5;
+                N = N % 5;
+                break;
+            } else {
+                N -= 2;
+                cnt++;
+            }
+        }
+        if (N == 0)
+            System.out.println(cnt);
+        else
+            System.out.println(-1);
+    }
+}
