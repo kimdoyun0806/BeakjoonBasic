711fdbd3f8a200aae66d397c22deb5ec0a114106
kimdoyun0806 <gim_doyun@naver.com>
2025-12-12 14:07:54 +0900

    동전 0

---
+++ Greedy/num_11047.java
@@ -0,0 +1,35 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.StringTokenizer;
+
+public class num_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int res = 0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            if (K >= arr[i]) {
+                res += K / arr[i];
+                K = K % arr[i];
+            }
+        }
+
+
+        System.out.println(res);
+    }
+}
