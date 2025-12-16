2ee291b2e555e49dcd3f1772a2c216b42a371b3c
kimdoyun0806 <gim_doyun@naver.com>
2025-12-16 16:57:19 +0900

    에너지드링크

---
+++ Greedy/num_20115.java
@@ -0,0 +1,30 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class num_20115 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        float[] arr = new float[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        float drink = arr[N - 1];
+
+        for (int i = 0; i < N - 1; i++) {
+            drink += arr[i] / 2;
+        }
+
+        System.out.println(drink);
+    }
+}
