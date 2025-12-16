8626232735e40354e122278c1366d55f55e3c6c8
kimdoyun0806 <gim_doyun@naver.com>
2025-12-16 18:06:50 +0900

    잃어버린 괄호

---
+++ Greedy/num_1541.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class num_1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), "-");
+        int res = Integer.MAX_VALUE;
+
+        while (st.hasMoreTokens()) {
+
+            int temp = 0;
+            StringTokenizer add = new StringTokenizer(st.nextToken(), "+");
+            while (add.hasMoreTokens()) {
+                temp += Integer.parseInt(add.nextToken());
+            }
+
+            if (res == Integer.MAX_VALUE)
+                res = temp;
+            else
+                res -= temp;
+        }
+        System.out.println(res);
+    }
+}
